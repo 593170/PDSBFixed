c8266550097cab7f987795554f4abb2fc953e87d
593170 <39775732+593170@users.noreply.github.com>
2018-09-04 20:06:40 -0400

    Update content.js

--- PDSB BYOD Avatar Remover/content.js
+++ PDSB BYOD Avatar Remover/content.js
@@ -7,7 +7,7 @@ var namef = document.getElementsByClassName("n-f");
 var namel = document.getElementsByClassName("n-l");
 namef[0].innerHTML = "Follow on IG";
 namel[0].innerHTML = "@pdsbfixed"
-element[0].style.backgroundImage = "url('https://cdn.shopify.com/s/files/1/1061/1924/files/Thinking_Face_Emoji.png?9898922749706957214')";
+element[0].style.backgroundImage = "url('https://github.com/593170/PDSBFixed/blob/master/pdsbfixed.png?raw=true')";
 element[0].style.backgroundColor = "transparent";
 
 
